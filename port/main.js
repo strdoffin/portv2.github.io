8ecac82590ba6c024eeecfdf1f5232054206343e
strdoffin <115920771+strdoffin@users.noreply.github.com>
2022-12-04 19:59:36 +0700

    Add files via upload

---
+++ port/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const hamburger_icon = hamburger.querySelector('span');
+const mobile_menu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click',()=>{
+    hamburger_icon.innerText = hamburger_icon.innerText === 'menu'
+        ? 'close'
+        : 'menu';
+    mobile_menu.classList.toggle('is-open');
+    mobile_menu.style.display = mobile_menu.style.display === 'block'
+        ?'none'
+        :'block';
+})
